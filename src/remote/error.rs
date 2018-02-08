3581171f80b610b666b31ed7fb9404dbc7f1c8a9
leavehouse <heavelouse@gmail.com>
2018-02-07 23:34:43 -0600

    Move remote::Error to its own submodule

---
+++ src/remote/error.rs
@@ -0,0 +1,50 @@
+use git2;
+use ipld_git;
+use lmdb;
+use multihash;
+use std::env;
+use std::io;
+
+use ipfs_api;
+
+#[derive(Debug)]
+pub enum Error {
+    ApiError(ipfs_api::Error),
+    EnvVarError(env::VarError),
+    Git2Error(git2::Error),
+    IoError(io::Error),
+    LmdbError(lmdb::Error),
+    IpldGitError(ipld_git::Error),
+    MultihashError(multihash::Error),
+    InvalidCommand(String),
+}
+
+impl From<env::VarError> for Error {
+    fn from(e: env::VarError) -> Self {
+        Error::EnvVarError(e)
+    }
+}
+
+impl From<git2::Error> for Error {
+    fn from(e: git2::Error) -> Self {
+        Error::Git2Error(e)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error::IoError(e)
+    }
+}
+
+impl From<lmdb::Error> for Error {
+    fn from(e: lmdb::Error) -> Self {
+        Error::LmdbError(e)
+    }
+}
+
+impl From<multihash::Error> for Error {
+    fn from(e: multihash::Error) -> Self {
+        Error::MultihashError(e)
+    }
+}
--- src/remote/mod.rs
+++ src/remote/mod.rs
@@ -8,51 +8,11 @@ use std::env;
 use std::fs;
 use std::io;
 
+pub use self::error::Error;
 use ipfs_api;
 
+mod error;
 mod tracker;
-
-#[derive(Debug)]
-pub enum Error {
-    ApiError(ipfs_api::Error),
-    EnvVarError(env::VarError),
-    Git2Error(git2::Error),
-    IoError(io::Error),
-    LmdbError(lmdb::Error),
-    IpldGitError(ipld_git::Error),
-    MultihashError(multihash::Error),
-    InvalidCommand(String),
-}
-
-impl From<env::VarError> for Error {
-    fn from(e: env::VarError) -> Self {
-        Error::EnvVarError(e)
-    }
-}
-
-impl From<git2::Error> for Error {
-    fn from(e: git2::Error) -> Self {
-        Error::Git2Error(e)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Self {
-        Error::IoError(e)
-    }
-}
-
-impl From<lmdb::Error> for Error {
-    fn from(e: lmdb::Error) -> Self {
-        Error::LmdbError(e)
-    }
-}
-
-impl From<multihash::Error> for Error {
-    fn from(e: multihash::Error) -> Self {
-        Error::MultihashError(e)
-    }
-}
 
 fn log_and_print(s: &str) {
    debug!("git <- '{}'", s);
